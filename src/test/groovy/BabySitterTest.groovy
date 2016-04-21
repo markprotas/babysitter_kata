14a8210073cdd208217d616f6dbe700924071ed4
Mark Protas <mark@nurturecode.com>
2016-04-21 18:39:38 -0400

    DRY up our tests by adding a utility method to perform iso 8601 string to date parsing

--- src/test/groovy/BabySitterTest.groovy
+++ src/test/groovy/BabySitterTest.groovy
@@ -5,11 +5,8 @@ class BabySitterTest extends Specification {
   def "When a baby sitter works from 5-6 PM, he or she should be paid \$12"() {
     given: "A baby sitter"
       def babySitter = new BabySitter()
-      def isoDateTimeParser = ISODateTimeFormat.dateTimeParser()
     when: "The baby sitter works from 5-6 PM"
-      def startTime = isoDateTimeParser.parseLocalDateTime('2016-04-01T17:00:00-05')
-      def endTime = isoDateTimeParser.parseLocalDateTime('2016-04-01T18:00:00-05')
-      def paymentDue = babySitter.calculatePaymentDue(startTime,endTime)
+      def paymentDue = buildDatesAndCalculatePayment(babySitter,'2016-04-01T17:00:00-05','2016-04-01T18:00:00-05')
     then: "He or she should be paid \$12"
       paymentDue == 12
   }
@@ -18,11 +15,8 @@ class BabySitterTest extends Specification {
     IllegalArgumentException should be thrown"""() {
     given: "A baby sitter"
       def babySitter = new BabySitter()
-      def isoDateTimeParser = ISODateTimeFormat.dateTimeParser()
     when: "The baby sitter's start time falls before their end time"
-      def startTime = isoDateTimeParser.parseLocalDateTime('2016-04-01T18:00:00-05')
-      def endTime = isoDateTimeParser.parseLocalDateTime('2016-04-01T17:00:00-05')
-      def paymentDue = babySitter.calculatePaymentDue(startTime,endTime)
+      def paymentDue = buildDatesAndCalculatePayment(babySitter,'2016-04-01T18:00:00-05','2016-04-01T17:00:00-05')
     then: "An IllegalArgumentException should be thrown"
       thrown IllegalArgumentException
   }
@@ -30,12 +24,18 @@ class BabySitterTest extends Specification {
   def "A baby sitter should not start their shift before 5 PM"() {
     given: "A baby sitter"
       def babySitter = new BabySitter()
-      def isoDateTimeParser = ISODateTimeFormat.dateTimeParser()
     when: "The baby sitter's start time is before 5 PM"
-      def startTime = isoDateTimeParser.parseLocalDateTime('2016-04-01T16:00:00-05')
-      def endTime = isoDateTimeParser.parseLocalDateTime('2016-04-01T18:00:00-05')
-      def paymentDue = babySitter.calculatePaymentDue(startTime,endTime)
+      def paymentDue = buildDatesAndCalculatePayment(babySitter,'2016-04-01T16:00:00-05','2016-04-01T18:00:00-05')
     then: "An IllegalArgumentException should be thrown"
       thrown IllegalArgumentException
+  }
+
+  def buildDatesAndCalculatePayment(BabySitter babySitter, 
+                                    String startTimeISO8601, 
+                                    String endTimeISO8601) {
+      def isoDateTimeParser = ISODateTimeFormat.dateTimeParser()
+      def startTime = isoDateTimeParser.parseLocalDateTime(startTimeISO8601)
+      def endTime = isoDateTimeParser.parseLocalDateTime(endTimeISO8601)
+      babySitter.calculatePaymentDue(startTime,endTime)
   }
 }
